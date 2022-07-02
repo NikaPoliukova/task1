21cbc5b0bca71a98fe9c19cb848e589ee4dfd626
NikaPoliukova <nikanarchuk@gmail.com>
2022-07-02 16:27:32 +0400

    Hello for pull request

--- task1/src/com.example/HelloTeach.java
+++ task1/src/com.example/HelloTeach.java
@@ -3,5 +3,6 @@ package com.example;
 public class HelloTeach {
     public static void main(String[] args) {
         System.out.println("HelloTeach");
+        System.out.println("Hello for pull request");
     }
 }
