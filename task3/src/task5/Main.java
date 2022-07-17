572cb68ff84a11d1e77cf0f4203f12c686cbb6a1
NikaPoliukova <nikanarchuk@gmail.com>
2022-07-17 23:29:51 +0400

    Lesson 5,ArraysCube,matrix and chess

---
+++ task3/src/task5/Main.java
@@ -0,0 +1,12 @@
+package task5;
+
+public class Main {
+    public static void main(String[] args) {
+        Chess.execute();
+        ArrayCube.execute();
+        TwoDimensionalArray.execute();
+        ArrayPrintDiagonal.execute();
+        ArraySort.execute();
+        ArraysMultiplication.execute();
+    }
+}
